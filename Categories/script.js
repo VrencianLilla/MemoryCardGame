16842ee024e1e5d471cb8e8ee1204ce97bb04864
Lilla Vrencian <vrencianlilla@gmail.com>
2026-02-07 11:12:28 +0200

    Repetitive code is now fixed (everything is cleaner now and there are fewer files now), renaming every id and class variables into english, now the game is available on Phones

--- Categories/script.js
+++ Categories/script.js
@@ -1,6 +1,6 @@
 class AudioController {
-  constructor() {
-    this.bgMusic = new Audio('Music/Heize.mp3');
+  constructor(bgMusicPath) {
+    this.bgMusic = new Audio(bgMusicPath);
     this.flipSound = new Audio('Music/flip.wav');
     this.matchSound = new Audio('Music/match.wav');
     this.victorySound = new Audio('Music/victory.mp3');
@@ -35,13 +35,13 @@ class AudioController {
 }
 
 class MixOrMatch {
-  constructor(totalTime, cards) {
+  constructor(totalTime, cards, bgMusicPath) {
     this.cardsArray = cards;
     this.totalTime = totalTime;
     this.timeRemaining = totalTime;
     this.timer = document.getElementById('time-remaining');
-    this.ticker = document.getElementById('megtalalt-kartyak');
-    this.audioController = new AudioController();
+    this.ticker = document.getElementById('total-cards-found');
+    this.audioController = new AudioController(bgMusicPath);
   }
 
   startGame() {
@@ -157,16 +157,75 @@ class MixOrMatch {
 
 }
 
+const imageSets = {
+  kdrama: {
+    images: [
+    "City_Hunter.jpg",
+    "W.jpg",
+    "Hotel_Del_Luna.jpg",
+    "Doctor_Strange.jpg",
+    "Itaewon_Class.jpg",
+    "Vincenzo.jpg",
+    "dodosolsollalasol.jpg",
+    "blood.jpg",
+    "ramen_shop.jpg",
+    "Her_Private_Life.jpeg",
+    "Romance_is_a_bonus_book.jpg",
+    "love_alarm.jpg"
+  ],
+  music: "Music/Heize.mp3"
+},
+  kpop: {
+    images: [
+    "bts.jpg",
+    "nct127.jpg",
+    "nuest.jpg",
+    "red_velvet.jpg",
+    "itzy.jpg",
+    "stray_kids.jpg",
+    "ateez.jpg",
+    "black_pink.jpg",
+    "exo1.jpg",
+    "got7.jpg",
+    "monstax.jpg",
+    "tripleH.jpg"
+    ],
+    music: "Music/Zico.mp3"
+  }
+};
+
+// detect category from html
+const category = document.body.dataset.category;
+const cardImages = imageSets[category].images;
+const bgMusicPath = imageSets[category].music;
+
+// Generate Cards
+let cards = [...cardImages, ...cardImages];
+cards = cards.sort(() => Math.random() - 0.5);
+
+const cardsContainer = document.getElementById("cards");
+cards.forEach(image => { 
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = ` 
+    <div class="card-back card-face"> 
+      <img class="cards-background-back-page" src="../Images/back.jpg">
+    </div> <div class="card-front card-face"> 
+      <img class="card-value" src="../Images/${image}">
+    </div> `;
+  cardsContainer.appendChild(card); 
+});
+
 function ready() {
   let overlays = Array.from(document.getElementsByClassName('overlay-text'));
   let cards = Array.from(document.getElementsByClassName('card'));
-  let game = new MixOrMatch(200, cards);
+  let game = new MixOrMatch(200, cards, bgMusicPath);
 
   // MUTE BUTTON 
   const muteBtn = document.getElementById("mute-btn");
-  muteBtn.addEventListener("click", () => { 
+  muteBtn.addEventListener("click", () => {
     game.audioController.toggleMute();
-    muteBtn.textContent = game.audioController.bgMusic.muted ? "Unmute 🔊" : "Mute 🔇"; 
+    muteBtn.textContent = game.audioController.bgMusic.muted ? "Unmute 🔊" : "Mute 🔇";
   });
 
   overlays.forEach(overlay => {
